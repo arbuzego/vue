44c8327169780f8da581e7be6d84cd69687972a5
Egor <denifenten2014@mail.ru>
2021-07-08 16:52:55 +0500

    Add title if notes is empty

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const App = {
         this.inputValue = '';
       }
     },
+    toUpperCase(item) {
+      return item.toUpperCase();
+    },
     deleteNote(idx) {
       this.notes.splice(idx, 1);
     },
