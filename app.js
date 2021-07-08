b834528fcd343db3c264b1ab867d1efd8b001ed7
Egor <denifenten2014@mail.ru>
2021-07-08 18:58:17 +0500

    dinami style, v-mode, computed and watch

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const App = {
     };
   },
   methods: {
-    inputChange(event) {
-      this.inputValue = event.target.value;
-    },
     addNote() {
       if (this.inputValue != '') {
         this.notes.push(this.inputValue);
@@ -22,6 +19,18 @@ const App = {
     },
     deleteNote(idx) {
       this.notes.splice(idx, 1);
+    },
+  },
+  computed: {
+    countDoubleComputed() {
+      return this.notes.length * 2;
+    },
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) {
+        this.inputValue = '';
+      }
     },
   },
 };
